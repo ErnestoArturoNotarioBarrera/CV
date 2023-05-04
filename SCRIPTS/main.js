cd912ef1228c2a4fdec1f0b604a774d80b5cd5c0
Ernesto Notario <ernestonotario97@gmail.com>
2023-05-04 17:18:47 -0600

    Creando mi CV

---
+++ SCRIPTS/main.js
@@ -0,0 +1,4 @@
+document.querySelector('#ir-arriba').addEventListener('click', function() {
+  // Utiliza scrollTop para volver al principio de la página
+  document.documentElement.scrollTop = 0;
+});
